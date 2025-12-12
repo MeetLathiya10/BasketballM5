59362cb3c677cddff18519337357756b5bbc4ec3
Meet Lathiya <156479142+MeetLathiya10@users.noreply.github.com>
2025-12-12 16:26:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ from fastapi import Query
 from fastapi.staticfiles import StaticFiles
 
 
-api = BalldontlieAPI(api_key="c9f755a9-862c-48ad-b1d9-bd5c96abbc04")
+api = BalldontlieAPI(api_key="YOUR_API_KEY")
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-#my api - c9f755a9-862c-48ad-b1d9-bd5c96abbc04
 
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
